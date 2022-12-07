67c29b1f5fcd4e4228831c88d3ffbb80b4f27cb7
chotsourian.marcos <chotsourian.marcos@gmail.com>
2022-12-07 15:38:41 +0100

    Starting Security Proj

---
+++ Security/Services/PasswordService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security.Services
+{
+    public class PasswordService
+    {
+        public bool IsValidPassword(string password)
+        {
+            //not implemented
+            return true;
+        }
+        public string GenerateNewRandomPasswordEncrypted()
+        {
+            //not implemented
+            return "";
+        }
+        private string EncryptPassword(string password)
+        {
+            //not implemented
+            return "";
+        }
+        private string DecryptPassword(string encryptedPassword)
+        {
+            //not implemented
+            return "";
+        }
+        public bool IsCorrectPassword(string loginName, string password)
+        {
+            //not implemented
+            return true;
+        }
+    }
+}
